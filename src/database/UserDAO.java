d7424e7c907a5791e30a62f262b52dd3f197a371
Md Azhan <azhansarfaraz870@gmail.com>
2026-05-25 23:03:06 +0530

    Connected registration with MySQL database

--- src/database/UserDAO.java
+++ src/database/UserDAO.java
@@ -1,5 +1,32 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class UserDAO {
-    
+
+    public boolean registerUser(
+            String username,
+            String password
+    ){
+        try{
+
+            Connection conn = DBConnection.getConnection();
+
+            String query = "INSERT INTO users(username,password) VALUES(?,?)";
+
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ps.executeUpdate();
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            
+            return false;
+        }
+    }
 }
--- src/gui/RegisterFrame.java
+++ src/gui/RegisterFrame.java
@@ -1,5 +1,6 @@
 package gui;
 
+import database.UserDAO;
 import java.awt.*;
 import javax.swing.*;
 
@@ -7,6 +8,10 @@ public class RegisterFrame extends JFrame {
 
     JButton registerButton;
     JButton backButton;
+
+    JTextField usernameField;
+    JPasswordField passwordField;
+    JPasswordField confirmField;
 
     public RegisterFrame() {
 
@@ -16,81 +21,188 @@ public class RegisterFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS)
+        mainPanel.setLayout(
+                new BoxLayout(mainPanel, BoxLayout.Y_AXIS)
         );
 
         JLabel title = new JLabel("REGISTER");
-        title.setFont(new Font("Arial", Font.BOLD,35)
+        title.setFont(
+                new Font("Arial", Font.BOLD,35)
         );
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        JLabel subtitle = new JLabel("Create your account");
-        subtitle.setFont(new Font("Arial",Font.PLAIN,15)
+        JLabel subtitle =
+                new JLabel("Create your account");
+
+        subtitle.setFont(
+                new Font("Arial", Font.PLAIN,15)
         );
 
         subtitle.setForeground(Color.DARK_GRAY);
-        subtitle.setAlignmentX(Component.CENTER_ALIGNMENT);
-        
-        JPanel formWrapper = new JPanel();
-        formWrapper.setLayout(new FlowLayout());
-
-        JPanel formPanel = new JPanel(new GridLayout(3,2,10,10)
+        subtitle.setAlignmentX(
+                Component.CENTER_ALIGNMENT
         );
 
-        formPanel.setPreferredSize(new Dimension(320,140));
+        JPanel formWrapper = new JPanel(
+                new FlowLayout()
+        );
+
+        JPanel formPanel = new JPanel(
+                new GridLayout(3,2,10,10)
+        );
+
+        formPanel.setPreferredSize(
+                new Dimension(320,140)
+        );
 
         JLabel l1 = new JLabel("Username :");
-        JTextField t1 = new JTextField();
+        usernameField = new JTextField();
 
         JLabel l2 = new JLabel("Password :");
-        JPasswordField p1 = new JPasswordField();
+        passwordField = new JPasswordField();
 
         JLabel l3 = new JLabel("Confirm Password :");
-        JPasswordField p2 = new JPasswordField();
+        confirmField = new JPasswordField();
+
+        formPanel.add(l1);
+        formPanel.add(usernameField);
+
+        formPanel.add(l2);
+        formPanel.add(passwordField);
+
+        formPanel.add(l3);
+        formPanel.add(confirmField);
 
         formWrapper.add(formPanel);
-        formPanel.add(l1);
-        formPanel.add(t1);
-        formPanel.add(l2);
-        formPanel.add(p1);
-        formPanel.add(l3);
-        formPanel.add(p2);
 
-        registerButton = new JButton("Register");
+        registerButton =
+                new JButton("Register");
 
-        backButton = new JButton("Back");
+        backButton =
+                new JButton("Back");
 
-        Dimension buttonSize = new Dimension(150,40);
+        Dimension buttonSize =
+                new Dimension(150,40);
 
-        registerButton.setMaximumSize(buttonSize);
-        backButton.setMaximumSize(buttonSize);
-
-        registerButton.setAlignmentX(Component.CENTER_ALIGNMENT
+        registerButton.setMaximumSize(
+                buttonSize
         );
-        backButton.setAlignmentX(Component.CENTER_ALIGNMENT
+
+        backButton.setMaximumSize(
+                buttonSize
+        );
+
+        registerButton.setAlignmentX(
+                Component.CENTER_ALIGNMENT
+        );
+
+        backButton.setAlignmentX(
+                Component.CENTER_ALIGNMENT
         );
 
         backButton.addActionListener(
-            e -> {
-                new WelcomeFrame();
-                dispose();
-            }
+                e -> {
+                    new WelcomeFrame();
+                    dispose();
+                }
+        );
+
+        registerButton.addActionListener(
+                e -> {
+
+                    String username =
+                            usernameField.getText();
+
+                    String password =
+                            String.valueOf(
+                                    passwordField.getPassword()
+                            );
+
+                    String confirmPassword =
+                            String.valueOf(
+                                    confirmField.getPassword()
+                            );
+
+                    if(username.isEmpty()
+                            || password.isEmpty()){
+
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Fields cannot be empty"
+                        );
+
+                        return;
+                    }
+
+                    if(!password.equals(
+                            confirmPassword
+                    )){
+
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Passwords do not match"
+                        );
+
+                        return;
+                    }
+
+                    UserDAO dao =
+                            new UserDAO();
+
+                    boolean success =
+                            dao.registerUser(
+                                    username,
+                                    password
+                            );
+
+                    if(success){
+
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Registration Successful"
+                        );
+
+                        usernameField.setText("");
+
+                        passwordField.setText("");
+
+                        confirmField.setText("");
+                    }
+
+                    else{
+
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Registration Failed"
+                        );
+                    }
+                }
         );
 
         mainPanel.add(Box.createVerticalGlue());
+
         mainPanel.add(title);
-        mainPanel.add(Box.createVerticalStrut(30)
-        );
+
+        mainPanel.add(Box.createVerticalStrut(30));
+
         mainPanel.add(subtitle);
+
         mainPanel.add(Box.createVerticalStrut(20));
+
         mainPanel.add(formWrapper);
+
         mainPanel.add(Box.createVerticalStrut(20));
+
         mainPanel.add(registerButton);
+
         mainPanel.add(Box.createVerticalStrut(10));
+
         mainPanel.add(backButton);
+
         mainPanel.add(Box.createVerticalGlue());
 
         add(mainPanel);
+
         setVisible(true);
     }
 }
